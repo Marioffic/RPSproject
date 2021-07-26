6e4c5d6b098bbfaf3545074c59a993c9ebfabebb
Kei-j <3a4io2@gmail.com>
2021-07-25 22:58:09 -0500

    I commented everything out, but I got one part working!

--- RPS.js
+++ RPS.js
@@ -18,19 +18,97 @@ else {
 }
 Ok, now on to actual code!
 */
-function computerPlay(playerSelection, computerSelection) {
-    let Rps = [/rock/g, /paper/g, /scissors/g]
-    if (playerSelection === /rock/g || /paper/g || /scissors/g) {
-        computerSelection = Math.floor(Math.random() * 3);
-    };
+/*
+
+
+player = window.prompt("type rock, paper, or scissors please");
+computerPlay = function(){
+    let Rps = [/rock/g, /paper/g, /scissors/g];
+    Rps = Rps.sort((/rock/g, /paper/g, /scissors/g) => 0.5 - Math.random());
+    console.log(Rps[0])
+    return Rps[0]
+};
+
+
+*
+
+
+function playRound(playerSelection, computerSelection) {
+    /*-------- FAULTY CODE------
     Switch(playerSelection, computerSelection) {
         case playerSelection==/rock/g && computerSelection==/paper/g:
             console.log("you win!")
             break;
         case 
-    }
-    
-}
-/*I hit a road  block yesterday, after thinking about it I found the solution!
+    ---------FAULTY CODE-------
+    } I hit a road  block yesterday, after thinking about it I found the solution!
 I will commit my initial road block though so I can look back and see how I 
-was doing */
+was doing. As a result I have commented out the faulty code and am starting
+again*/
+
+/*
+
+--------------------------------------------------------------------
+
+    if (computerSelection === /rock/g) {
+        switch(playerSelection) {
+            case /rock/g:
+                return  "You tie! Rock ties with Rock";
+            case /paper/g:
+                return  "You Win! Paper beats Rock";
+            case /scissors/g:
+                return  "You Lose! Rock beats Scissors";
+        };
+    };
+    if (computerSelection === /paper/g) {
+        switch(playerSelection) {
+            case /rock/g:
+                return "You Lose! Paper beats Rock";
+            case /paper/g:
+                return "You tie! Paper ties with Paper";
+            case /scissors/g:
+                return "You Win! Scissors beats Paper";
+        };
+    };
+    if (computerSelection === /scissors/g){
+        switch(playerSelection) {
+            case /rock/g:
+                return  "You Win! Rock beats Scissors";
+            case /paper/g:
+                return  "You Lose! Scissors beats Paper";
+            case /scissors/g:
+                return  "You tie! Scissors ties with Scissors";
+        };
+    };
+    
+};
+
+
+
+
+function game() {
+    playRound(player,computerPlay);
+    playRound(player ,computerPlay);
+    playRound(player,computerPlay);
+    playRound(player,computerPlay);
+    playRound(player,computerPlay);
+};
+
+*/ 
+
+/* LOL So as we will see with this commit, I had to comment out everything,
+it was a mess, but I realized I wasn't dividing and conquering. My goal was 
+simply 'kill the beast' rather than starting with something like 'take out its
+legs first!' so I went back to the drawing board, and our opponent can now 
+spit our pseudo-random rock paper scissors choices, lets go!*/
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+function computerPlay() {
+    let RPS = ['rock', 'paper', 'scissors'];
+    let Randomizer = getRandomInt(3);
+    let RanRPS = RPS[Randomizer]
+    return RanRPS;
+
+};
+console.log(computerPlay())
